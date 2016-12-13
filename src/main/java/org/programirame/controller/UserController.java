bcbbd1e2a1718644fb197db0b0e39e30d6345380
Igor Stojanovski <igorce@outlook.com>
2016-12-13 21:40:28 +0100

    Service for creating a user

---
+++ src/main/java/org/programirame/controller/UserController.java
@@ -0,0 +1,42 @@
+package org.programirame.controller;
+
+
+import org.programirame.models.User;
+import org.programirame.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@ExposesResourceFor(User.class)
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    EntityLinks entityLinks;
+
+    /**
+     * Creates a {@link User} and returns the newly created object.
+     *
+     * @param user The new User resource to be created.
+     * @return Newly created {@link User} object.
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity registerNewUser(@RequestBody User user) {
+
+        Resource<User> resource = new Resource<>(userService.createUser(user));
+        resource.add(entityLinks.linkToSingleResource(User.class, resource.getContent().getId()));
+        return new ResponseEntity<>(resource, HttpStatus.CREATED);
+    }
+
+}
---
+++ src/main/java/org/programirame/services/UserService.java
@@ -0,0 +1,17 @@
+package org.programirame.services;
+
+import org.programirame.models.User;
+import org.programirame.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
