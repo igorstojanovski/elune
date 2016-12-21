e4fb73add0a1824fe2c27c8024287a89ca8a9d5e
Igor Stojanovski <igorce@outlook.com>
2016-12-21 23:17:13 +0100

    Service to retrieve all users
    Service that retrieves all the users without filtering or ordering. Password is omitted from the response.

--- src/main/java/org/programirame/services/UserService.java
+++ src/main/java/org/programirame/services/UserService.java
@@ -5,7 +5,6 @@ import org.programirame.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 @Service
