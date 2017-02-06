d967806b5e617f7539f9cea2bbe3f23c69f07d5e
Igor Stojanovski <igorce@outlook.com>
2017-02-06 20:33:00 +0100

    Add testing constants class
    Added a testing constant class that should
    centralize all testing values in one place.

---
+++ src/test/java/org/programirame/services/events/TestingConstants.java
@@ -0,0 +1,17 @@
+package org.programirame.services.events;
+
+public class TestingConstants {
+    public static final String TIME_08_00 = "08:00";
+    public static final String TIME_08_10 = "08:10";
+    public static final String TIME_07_00 = "07:00";
+    public static final String TIME_09_00 = "09:00";
+    public static final String TIME_17_00 = "17:00";
+
+    public static final String DATE_02_27 = "2017-02-27";
+    public static final String DATE_02_28 = "2017-02-28";
+
+    public static final String NAME = "igor";
+    public static final String SURNAME = "stojanovski";
+    public static final String USERNAME = "igorce";
+    public static final String PASSWORD = "igorcepass";
+}
